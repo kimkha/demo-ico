0bee27890b1b2a6fe5321130cb89cadf50f9119e
Nguyễn Kim Kha <nkimkha@gmail.com>
2017-10-15 16:34:38 +0800

    Init

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 var KhaToken2 = artifacts.require("./KhaToken2.sol");
 
 module.exports = function(deployer) {
-  // web3.personal.unlockAccount(web3.eth.accounts[0], 'ThuSuong@400');
+  // web3.personal.unlockAccount(web3.eth.accounts[0], '');
   deployer.deploy(KhaToken2);
   // deployer.deploy(GustavoCoin);
 };
